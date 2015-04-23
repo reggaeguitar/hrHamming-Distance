a5a27b8575b3c1ffd3708d805043e58b2b998654
Joseph Brady <escalade2005@hotmail.com>
2015-04-22 20:29:04 -0500

    Added test case analyzer

---
+++ TestCaseAnalyzer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCaseAnalyzer
+{
+    class Program
+    {
+        static void Main()
+        {
+            var casesToAnalyze = new [] {21};
+            foreach (var caseNum in casesToAnalyze)
+            {
+                var inputLines = File.ReadAllLines(
+                    String.Format(@"Inputs\{0}.txt", caseNum));
+                var inputLinesPastHeader = inputLines.Skip(3);
+                var commands = inputLinesPastHeader.GroupBy(x => x[0]);
+                var counts = new List<KeyValuePair<char, int>>();
+                foreach (var command in commands)
+                {
+                    counts.Add(new KeyValuePair<char, int>(command.Key, command.Count()));   
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
